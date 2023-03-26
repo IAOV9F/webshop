cdcc2a992bb88a310c951216cb8acc26be77667d
IAOV9F <keresztestimi99@gmail.com>
2023-03-26 15:41:56 +0200

    created first look and impoved navigation bar

---
+++ script.js
@@ -0,0 +1,8 @@
+const wrapper = document.querySelector(".sliderWrapper")
+const menuItems = document.querySelectorAll(".menuItem")
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click", ()=>{
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+});
